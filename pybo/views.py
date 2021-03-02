b07b834b4d46963225ff6dc81980eaec0dd14450
kimwram <kimwheeram@naver.com>
2021-03-02 20:01:07 +0900

    세번째 수업

--- pybo/views.py
+++ pybo/views.py
@@ -4,16 +4,23 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 
 def index(request):
     """
     pybo 목록 출력
     """
+    page = request.GET.get('page', '1')  # 페이지
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
+    # context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
     # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+
+
 
 def detail(request, question_id):
     """
@@ -55,4 +62,5 @@ def question_create(request):
     else:
         form = QuestionForm()
     context = {'form': form}
+
     return render(request, 'pybo/question_form.html', context)
