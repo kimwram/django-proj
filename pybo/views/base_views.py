9ce74880f7f426789c9b14fc4c818ee9c34c5dc8
kimwram <kimwheeram@naver.com>
2021-03-05 18:32:10 +0900

    다섯번째 수업

---
+++ pybo/views/base_views.py
@@ -0,0 +1,28 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+
+from ..models import Question
+
+
+def index(request):
+    """
+    pybo 목록 출력
+    """
+    page = request.GET.get('page', '1')  # 페이지
+    question_list = Question.objects.order_by('-create_date')
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
+    # context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+    # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+
+
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
